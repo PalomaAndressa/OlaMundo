19bc24db0c1ccffcead324eeaaee50560d7da6e5
Auto Logon <autologon@CSED.LAB>
2024-08-13 21:20:09 -0300

    codigo da aula

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+string = nota1;
+string = nota2;
+
+Console.WriteLine("Hello, World!");
+Consoleonsole.WriteLine("Informe o valor da nota 1");
+ var.nota1 = Console.ReadLine();
+Console.WriteLine("informe o valor da nota 2"):
+ var.nota2 = Console.ReadLine();
+ 
+Console.WriteLine($"Nota 1: {nota1}");
+console.WriteLine($"Nota 2: {nota2}");
